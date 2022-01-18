e2e24812972fcd7327a7f6997ba15229e862f38f
garlapatisatish <satish@airtime.com>
2022-01-18 10:52:15 -0800

    moving zip from repo to releases

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "cybertron",
-            url: "https://github.com/garlapatisatish/Cybertron_test/blob/main/cybertron.xcframework.zip",
+            url: "https://github.com/garlapatisatish/Cybertron_test/releases/download/v1.0.0/cybertron.xcframework.zip",
             checksum: "473a25a4b9e87a2fcfdc2510ab23754d520d05c856f397f0c0de6213badbf854"
         ),
     ]
